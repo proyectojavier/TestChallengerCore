cb3da8d1bb117b6f4fad3ae563b3599463c18393
Javier gonzalez <fjarmas91@gmail.com>
2021-02-18 21:18:22 -0600

    Cambio 3

---
+++ Models/SignUpModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChallengerCore.Models
+{
+    public class SignUpModel
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
