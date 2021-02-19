393e0ac69da22bd810b40dcb813a72aa584e2f67
Javier gonzalez <fjarmas91@gmail.com>
2021-02-18 22:53:53 -0600

    Sexto cambio

--- Controllers/CuentaLoginController.cs
+++ Controllers/CuentaLoginController.cs
@@ -1,8 +1,11 @@
 ﻿using ChallengerCore.Models;
 using Firebase.Auth;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -38,15 +41,112 @@ namespace ChallengerCore.Controllers
             return View();
         }
         // GET: CuentaLogin/Edit/5
-        public ActionResult Edit(int id)
+        
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login(string returnURL)
         {
+            try
+            {
+                if (Request.IsAuthenticated)
+                {
+                    return RedirectToRoute(returnURL);
+                }
+            }catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
             return View();
         }
 
+        [AllowAnonymous]
+        [HttpPost]
+
+        public async Task<ActionResult> Login(LoginModel model, string returnURL)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var auth = new FirebaseAuthProvider(new FirebaseConfig(apiKEY));
+                    var a = await auth.SignInWithEmailAndPasswordAsync(model.Email, model.Password);
+                    string token = a.FirebaseToken;
+                    var user = a.User;
+                    if (token != "")
+                    {
+                        SignInUser(user.Email, token, false);
+                        return RedirectToLocal(returnURL);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Error username or password.");
+                    }
+                }
+
+            }catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return View();
+        }
         // POST: CuentaLogin/Edit/5
-      
+        private void SignInUser(string email, string token, bool isPersistent)
+        {
+            // Initialization.
+            var claims = new List<Claim>();
+
+            try
+            {
+                // Setting
+                claims.Add(new Claim(ClaimTypes.Email, email));
+                claims.Add(new Claim(ClaimTypes.Authentication, token));
+                var claimIdenties = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+                var ctx = Request.GetOwinContext();
+                var authenticationManager = ctx.Authentication;
+                // Sign In.
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, claimIdenties);
+            }
+            catch (Exception ex)
+            {
+                // Info
+                throw ex;
+            }
+        }
+
+       
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            try
+            {
+                // Verification.
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    // Info.
+                    return Redirect(returnUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Info
+                throw ex;
+            }
+
+            // Info.
+            return this.RedirectToAction("LogOff", "Account");
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult LogOff()
+        {
+            var ctx = Request.GetOwinContext();
+            var authenticationManager = ctx.Authentication;
+            authenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Login", "CuentaLogin");
+        }
 
         // POST: CuentaLogin/Delete/5
-       
+
     }
 }
---
+++ StartUp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChallengerCore.App_Start.StartUpFB))]
+namespace ChallengerCore.App_Start
+{
+    public partial class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
